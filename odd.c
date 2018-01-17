f3b5fe56a25dd60d4030ca3e329a55aac5386b5a
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-17 16:28:29 +0530

    odd or even
    to check whether a number uis odd or even

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main
+{
+int x;
+printf("\n enter the number:");
+sacnf("\n %d",&x);
+if(x%2==0)
+{
+printf("\n the given number is even");
+else
+printf("\n the given number is negative");
+}
+getch();
+}

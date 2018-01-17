39ab5ef89f280a32985405d77521398225cb5298
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-17 16:41:26 +0530

    consonent
    vowel or consonent

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char fv;
+printf("\n enter the number");
+scanf("%c",&fv);
+if(fv==a,e,i,o,u)
+{
+printf("\n the given char is a vowel");
+else
+printf("\n the given char is not a consonent")'
+}
+getch():
+}

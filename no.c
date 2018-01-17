b448d05c654710e015a7c125c7c65e7ef69c802f
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-17 16:52:56 +0530

    alphabet
    print alphabet or no

---
+++ no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char fv;
+printf("\n enter a character:");
+scanf("%c",&ch);
+if(ch=='a'to'z'//'AA'TO'Z')
+{
+Printf("\n alphabet");
+else
+printf("\n no");
+}
+getch();
+}
